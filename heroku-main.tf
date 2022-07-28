b5bc39b7cfa3e58b9c75bcc348169e32ba3c0894
Marcelo Gomes <marcelosuptec@hotmail.com>
2022-07-28 18:32:40 -0300

    Mensagem

--- heroku-main.tf
+++ heroku-main.tf
@@ -1,8 +1,10 @@
+# Create a new Heroku app
 resource "heroku_app" "searaembu" {
   name   = "searaembu"
   region = "us"
 }
 
+# Create a database, and configure the app to use it
 resource "heroku_addon" "database" {
   app_id = heroku_app.searaembu.id
   plan   = "heroku-postgresql:hobby-dev"
